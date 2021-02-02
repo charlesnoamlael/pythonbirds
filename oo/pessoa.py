35d64faa3b87ab6c8f3b20f8b2f3fe49f5dadf0d
charlesnoamlael <nasa0147>
2021-02-02 15:28:03 -0300

    criado e removido atributo dinâmico de objetos do tipo pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -21,6 +21,31 @@
 
 # ATRIBUTOS COMPLEXO
 
+# class Pessoa():
+#     def __init__(self, *filhos, nome=None, idade=35):
+#         self.nome = nome
+#         self.idade = idade
+#         self.filhos = list(filhos)
+#
+#
+#     def cumprimentar(self):
+#         return f'Olá {id(self)}'
+#
+#
+# if __name__ == '__main__':
+#     renzo = Pessoa(nome='Renzo')
+#     luciano = Pessoa(renzo, nome='Luciano')
+#     print(Pessoa.cumprimentar(luciano))
+#     print(id(luciano))
+#     print(luciano.cumprimentar())
+#     print(luciano.nome)
+#     print(luciano.idade)
+#     for filho in luciano.filhos:
+#         print(filho.nome)
+
+
+# ATRIBUTO DINAMICO
+
 class Pessoa():
     def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
@@ -42,6 +67,12 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Ramanlho' # Criação do atributo dinâmico fazendo a atribuição
+    del luciano.filhos             # Remoção do atributo
+    print(luciano.__dict__)        # Os atributos de instancia ficam presentes no __dict__
+    print(renzo.__dict__)
+
+
 
 
 
